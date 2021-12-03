cbbc16b7222a381270f1fd417c1fd472e600160c
evebigaj <eve.bigaj@gmail.com>
2021-12-03 16:35:50 -0500

    create dictionaryFetcher

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,11 +10,10 @@ import Words from './Words'
 import Form from './Form'
 
 import {boggleAlphabet, initialBoard} from '../utils/board'
-import {existsStringPath} from '../utils/wordFinder'
+import {asyncFetchWord} from '../utils/dictionaryFetcher'
 import countPoints from '../utils/scoreCounter'
+import { existsStringPath } from '../utils/wordFinder';
 
-//you'll need to get your own key from https://dictionaryapi.com/products/api-collegiate-dictionary 
-import {key} from '../private/key'
 
 
 
@@ -45,31 +44,16 @@ const App = () => {
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(word.length >2 && existsStringPath(word.toUpperCase(), board) 
-    && !words.includes(word.toUpperCase())){
-    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${key}`)
-    .then(
-    response => {
-    if(response.ok){
-        return response.json()
-    }
-}
-).then( 
-    data => {
-      //for some non-existent words, 
-      //Merriam-Webster API returns array of words you might have meant
-      // so this conditional rules out that possibility
-      //because there typeof data[0]==="string"
-      if(typeof data[0] === "object"){
-    return true}
-      else{
-    return false}
-    }
-).then(truthValue => {if(truthValue ===true){
+  if(word.length >2 && existsStringPath(word.toUpperCase(), board) 
+  && !words.includes(word.toUpperCase())){
+   asyncFetchWord(word)
+   .then(truthValue => {
+     if(truthValue === true){
 words.push(word.toUpperCase());
       setWords(words);
       setPoints(countPoints(words));
-}}).catch(error => Promise.reject() );
+}})
+.catch(error => Promise.reject())};
 
 setWord('')}
 
@@ -80,7 +64,7 @@ setWord('')}
 //       setPoints(countPoints(words))
 //     }}
 //     setWord('')
-};
+
 
   return <>
   <div id="main">
---
+++ src/utils/dictionaryFetcher.js
@@ -0,0 +1,33 @@
+//you'll need to get your own key from https://dictionaryapi.com/products/api-collegiate-dictionary 
+import {key} from '../private/key'
+
+
+export const fetchWord = (word) => {
+  
+    
+   return fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${key}`)
+    .then(
+    response => {
+    if(response.ok){ 
+        return response.json()
+    }
+}
+).then( 
+    data => {
+      //for some non-existent words, 
+      //Merriam-Webster API returns array of words you might have meant
+      // so this conditional rules out that possibility
+      //because there typeof data[0]==="string"
+      if(typeof data[0] === "object"){
+    return true}
+      else{
+    return false}
+    })
+    .catch(error => Promise.reject())
+}
+
+
+export const asyncFetchWord = async (word) => {
+  const result = await fetchWord(word);
+  return result
+}
