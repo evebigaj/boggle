353db5343fb20a5586b6187a8df9d18bd4e9b7c4
evebigaj <89229822+evebigaj@users.noreply.github.com>
2021-12-27 10:44:26 -0500

    Update Form.js
    remove label

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,7 +4,6 @@ const Form = (props) => {
 
     return <>
     <form id="form" onSubmit={props.onSubmit}>
-      <label htmlFor="word">Enter Word: </label> <br/>
       <input type="text" id="word" value={props.word} 
       onChange = {props.handleChange} autocomplete="off"></input> <br/>
       <input type="submit" value="submit" id="submit"></input>
